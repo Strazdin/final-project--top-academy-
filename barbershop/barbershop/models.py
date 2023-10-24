fa1b744158da92d7ee0ce7f94cc70b83fbc14696
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-10-24 07:39:35 +0300

    3. Создал модель Price

--- barbershop/barbershop/models.py
+++ barbershop/barbershop/models.py
@@ -7,4 +7,10 @@ class Barbers(models.Model):
 
     def __str__(self):
         return self.barber_name
-  
+    
+class Price(models.Model):
+    service = models.CharField(max_length=200)
+    price = models.IntegerField(default=0, null=True, blank=True)
+
+    def __str__(self):
+        return self.service
