3eed8c435a705717873e98b9a8f37e3157969ca9
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-10-24 22:50:20 +0300

    5. Настроил отображение моделей

--- barbershop/barbershop/models.py
+++ barbershop/barbershop/models.py
@@ -8,9 +8,17 @@ class Barbers(models.Model):
     def __str__(self):
         return self.barber_name
     
+    class Meta:
+        verbose_name = 'Барбера'
+        verbose_name_plural = 'Барберы'
+    
 class Price(models.Model):
     service = models.CharField(max_length=200)
     price = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.service
+    
+    class Meta:
+        verbose_name = 'Стрижка'
+        verbose_name_plural = 'Прайс-лист'
