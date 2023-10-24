933cf110870cb4a3970e0f435024e98b4945961e
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-10-25 02:44:18 +0300

    6. Отобразил портфолио

--- barbershop/barbershop/views.py
+++ barbershop/barbershop/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from .models import Barbers, Price
+from .models import Barbers, Price, Portfolio
 
 def barbershop(request):
     barbers = Barbers.objects.all()
     price_list = Price.objects.all()
+    portfolio = Portfolio.objects.all()
 
     dict_obj = {
         'barbers': barbers,
         'price_list': price_list,
+        'portfolio': portfolio
     }
 
     return render(request, 'barbershop/barbershop.html', dict_obj)
