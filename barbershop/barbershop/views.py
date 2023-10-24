f811ba67652977fca114ebf4f273adb42962007f
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-10-25 02:01:06 +0300

    6. Отобразил прайс

--- barbershop/barbershop/views.py
+++ barbershop/barbershop/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from .models import Barbers
+from .models import Barbers, Price
 
 def barbershop(request):
     barbers = Barbers.objects.all()
-    return render(request, 'barbershop/barbershop.html', {'barbers': barbers})
+    price_list = Price.objects.all()
+
+    dict_obj = {
+        'barbers': barbers,
+        'price_list': price_list,
+    }
+
+    return render(request, 'barbershop/barbershop.html', dict_obj)
