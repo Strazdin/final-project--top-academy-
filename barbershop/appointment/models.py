19abf69eb5d77e781d16af404c6005393131e861
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-10-24 03:03:59 +0300

    2. Создал таблицу в БД

--- barbershop/appointment/models.py
+++ barbershop/appointment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Appointment(models.Model):
+    dt = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    barber = models.CharField(max_length=100, blank=True, null=True)
+    service = models.CharField(max_length=250, blank=True, null=True)
+    day = models.DateField()
+    time = models.TimeField()
+
+    def __sts__(self):
+        return self.name
