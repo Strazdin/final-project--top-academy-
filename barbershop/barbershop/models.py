892399caeb8db6489844305529203d5db08e8f12
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-10-24 05:58:24 +0300

    3. Создал модель Barbers

--- barbershop/barbershop/models.py
+++ barbershop/barbershop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Barbers(models.Model):
+    barber_name = models.CharField(max_length=100)
+    barber_image = models.ImageField(upload_to='barbers/images/')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+  
