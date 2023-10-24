9ed1500f9aec9fad845f786e57e8973046d3ac90
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-10-24 23:08:27 +0300

    5. Расширил отображение полей

--- barbershop/barbershop/admin.py
+++ barbershop/barbershop/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Barbers, Price
 # Register your models here.
 
-admin.site.register(Barbers)
+class BarbersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'barber_name', 'created')
+    list_display_links = ('id', 'barber_name')
+    search_fields = ('id', 'barber_name')
+
+admin.site.register(Barbers, BarbersAdmin)
 admin.site.register(Price)
--- barbershop/barbershop/apps.py
+++ barbershop/barbershop/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class BarbershopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'barbershop'
+    # verbose_name = 'Название парикмахерской'
+
