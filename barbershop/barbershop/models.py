b4ac4262d430c45303a5e73dcd22252cd95d6cd4
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-10-25 09:05:58 +0300

    7. Закончил работу с портфолио

--- barbershop/barbershop/models.py
+++ barbershop/barbershop/models.py
@@ -28,7 +28,7 @@ class Price(models.Model):
 class Portfolio(models.Model):
     image_small = models.ImageField(upload_to='portfolio/images/', blank=True, null=True, verbose_name="Фото маленькое")
     image_big = models.ImageField(upload_to='portfolio/images/', blank=True, null=True, verbose_name="Фото большое")
-    end_photo_title = models.CharField(max_length=200, blank=True, null=True, verbose_name="Конец названия фото")
+    end_photo_title = models.CharField(max_length=200, blank=True, null=True, verbose_name="Название большого фото")
     barber = models.ForeignKey(Barbers, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Барбер")
 
     class Meta:
