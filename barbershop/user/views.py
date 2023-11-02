c8880d7f91e4c825f575f4cc5755111915c038d2
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-11-02 17:14:38 +0300

    9. Добавил обработчик + миграции

--- barbershop/user/views.py
+++ barbershop/user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Profile
 # Create your views here.
+
+def profiles(request):
+    prof = Profile.objects.all()
+    context = {'profiles': prof}
+    return render(request, 'user/profile.html', context)
